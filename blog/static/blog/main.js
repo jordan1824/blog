9be34d146a15c2f54ced2c3038e3d766825de770
Jordan Deveau <jordandeveau@Jordans-Macbook.local>
2020-08-29 14:41:10 -0400

    Added asynchronous registration form errors

--- blog/static/blog/main.js
+++ blog/static/blog/main.js
@@ -94,3 +94,174 @@ if (document.querySelector(".comment-btn")) {
     }
   })
 }
+
+class FormErrorMessage {
+  constructor() {
+    this.registerFormFields = document.querySelectorAll(".register-field")
+    this.usernameCounter = 0
+    this.emailCounter = 0
+    this.passwordCounter = 0
+    this.passwordConfirmCounter = 0
+    this.firstNameCounter = 0
+    this.lastNameCounter = 0
+    if (this.registerFormFields) {this.events()}
+  }
+
+  // Events
+  events() {
+    this.registerFormFields.forEach(field => field.addEventListener("keyup", () => {
+      if (field.classList.contains("username")) {
+        this.usernameFieldHandler(field)
+      }
+      if (field.classList.contains("email")) {
+        this.emailFieldHandler(field)
+      }
+      if (field.classList.contains("password")) {
+        this.passwordFieldHandler(field)
+      }
+      if (field.classList.contains("password-confirm")) {
+        this.passwordConfirmFieldHandler(field)
+      }
+      if (field.classList.contains("first-name")) {
+        this.firstNameFieldHandler(field)
+      }
+      if (field.classList.contains("last-name")) {
+        this.lastNameFieldHandler(field)
+      }
+    }))
+  }
+
+  // Models
+
+  usernameFieldHandler(field) {
+    let errors = 0;
+    clearTimeout(this.usernameCounter)
+    if (field.value.length > 50) {errors = this.insertAlert(field, "Your username cannot exceed 50 characters.", errors)}
+    if (field.value.match(/[$&+,:;=?@#|'<>.^*()%!_-]+/)) {errors = this.insertAlert(field, "Your username cannot contain special characters.", errors)}
+    this.usernameTimedChecks(field, errors)
+    if (!errors) {
+      this.removeAlert(field)
+    }
+  }
+
+  usernameTimedChecks(field, errors) {
+    if (field.value.length > 0 && field.value.length < 4) {
+      this.usernameCounter = setTimeout(() => errors = this.insertAlert(field, "Your username must be atleast 4 characters long.", errors), 2000)
+    }
+    if (field.value.length == 0) {
+      this.usernameCounter = setTimeout(() => errors = this.insertAlert(field, "You must enter a valid username.", errors), 2000)
+    }
+  }
+
+  emailFieldHandler(field) {
+    let errors = 0;
+    clearTimeout(this.emailCounter)
+    if (field.value.length > 200) {errors = this.insertAlert(field, "Your email cannot exceed 200 characters.", errors)}
+    this.emailTimedChecks(field, errors)
+    if (!errors) {
+      this.removeAlert(field)
+    }
+  }
+
+  emailTimedChecks(field, errors) {
+    if (!field.value.match(/[A-Za-z0-9]+@[a-zA-Z]+\.[a-zA-Z]+/)) {
+      this.emailCounter = setTimeout(() => errors = this.insertAlert(field, "You must provide a valid email address.", errors), 3000)
+    }
+  }
+
+  passwordFieldHandler(field) {
+    let errors = 0;
+    clearTimeout(this.passwordCounter)
+    if (field.value.length > 50) {errors = this.insertAlert(field, "Your password cannot exceed 50 characters.", errors)}
+    this.passwordTimedChecks(field, errors)
+    if (!errors) {
+      this.removeAlert(field)
+    }
+  }
+
+  passwordTimedChecks(field, errors) {
+    if (field.value.length > 0 && field.value.length < 8) {
+      this.passwordCounter = setTimeout(() => errors = this.insertAlert(field, "Your password must be atleast 8 characters long.", errors), 2000)
+    }
+    if (field.value.length == 0) {
+      this.passwordCounter = setTimeout(() => errors = this.insertAlert(field, "You must enter a valid password.", errors), 2000)
+    }
+  }
+
+  passwordConfirmFieldHandler(field) {
+    let errors = 0;
+    clearTimeout(this.passwordConfirmCounter)
+    this.passwordConfirmTimedChecks(field, errors)
+    if (!errors) {
+      this.removeAlert(field)
+    }
+  }
+
+  passwordConfirmTimedChecks(field, errors) {
+    if (field.value != document.querySelector(".password").value) {
+      this.passwordConfirmCounter = setTimeout(() => errors = this.insertAlert(field, "Your passwords do not match.", errors), 2000)
+    }
+  }
+
+  firstNameFieldHandler(field) {
+    let errors = 0;
+    clearTimeout(this.firstNameCounter)
+    if (field.value.length > 250) {errors = this.insertAlert(field, "Your first name cannot exceed 250 characters.", errors)}
+    this.firstNameTimedChecks(field, errors)
+    if (!errors) {
+      this.removeAlert(field)
+    }
+  }
+
+  firstNameTimedChecks(field, errors) {
+    if (field.value.length == 0) {
+      this.firstNameCounter = setTimeout(() => errors = this.insertAlert(field, "You must enter a valid first name.", errors), 2000)
+    }
+  }
+
+  lastNameFieldHandler(field) {
+    let errors = 0;
+    clearTimeout(this.lastNameCounter)
+    if (field.value.length > 250) {errors = this.insertAlert(field, "Your last name cannot exceed 250 characters.", errors)}
+    this.lastNameTimedChecks(field, errors)
+    if (!errors) {
+      this.removeAlert(field)
+    }
+  }
+
+  lastNameTimedChecks(field, errors) {
+    if (field.value.length == 0) {
+      this.lastNameCounter = setTimeout(() => errors = this.insertAlert(field, "You must enter a valid last name.", errors), 2000)
+    }
+  }
+
+  insertAlert(field, message, errors) {
+    errors += 1;
+    if (!field.parentElement.querySelector(".alert")) {
+      field.parentElement.insertAdjacentHTML("afterbegin", this.alertHTML(message))
+      field.parentElement.querySelector(".alert").addEventListener("animationend", function() {
+        this.classList.remove("error-popup-animation")
+    })
+    }
+    return errors
+  }
+
+  alertHTML(error) {
+    return `<div class='alert error-popup-animation'>
+      <p class='error-message'>${error}</p>
+    </div>`
+  }
+
+  removeAlert(field) {
+    if (field.parentElement.querySelector(".alert")) {
+      let currentFieldAlert = field.parentElement.querySelector(".alert")
+      currentFieldAlert.classList.add("reverse-error-popup-animation")
+      currentFieldAlert.addEventListener("animationend", function() {
+        currentFieldAlert.remove()
+      })
+    }
+  }
+
+}
+
+new FormErrorMessage()
