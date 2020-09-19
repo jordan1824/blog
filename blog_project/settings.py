17bca0d38f4eb1c0950f21c0fa71ba292763e063
Jordan Deveau <jordandeveau@Jordans-Macbook.local>
2020-09-19 14:03:55 -0400

    Updated settings again

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
