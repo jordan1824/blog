7fdcf254c2c14a53aefff4030502c67f56238036
Jordan Deveau <jordandeveau@Jordans-Macbook.local>
2020-10-01 18:17:09 -0400

    Added allowed hosts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ['socialsharingapp.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['socialsharingapp.herokuapp.com', 'localhost', 'www.socialapp.jordandeveau.ca', 'socialapp.jordandeveau.ca']
 
 
 # Application definition
